b968ee9cad4ec56fc1c540ae43f425248455ec9c
unknown <phanipolavarapu15@gmail.com>
2025-03-01 21:45:44 +0530

    few changes

--- JS Files/SelectChat.js
+++ JS Files/SelectChat.js
@@ -1,19 +1,17 @@
 
 
 function openChat(index) {
-    let chatPage = document.getElementById('chatSpace');
+    let chatPage = document.getElementById('ChatHead');
+    chatPage.classList.add("bg-[#21012A]",  "flex")
     chatPage.innerHTML = ""
-    let head = document.createElement("div");
-    head.classList.add("bg-[#21012A]", "h-16", "w-[75vw]", "flex")
     let image = document.createElement('img');
     image.src = "../images/UserIcon.png"
     image.classList.add('size-[45px]', 'mx-5', "mt-2");
     let div = document.createElement("div");
     div.textContent = names[index]
     div.classList.add('text-2xl', 'ml-5', "text-white", "mt-4")
-    head.appendChild(image)
-    head.appendChild(div)
-    chatPage.appendChild(head)
+    chatPage.appendChild(image)
+    chatPage.appendChild(div)
 }
 
 // Chat Names Array
@@ -39,13 +37,25 @@ names.forEach((name, index) => {
     li.addEventListener("click", () => openChat(index))
 })
 
+// function to check names are same or not
+function isEqualIgnoreCase(str1,str2){
+    let st1 = str1.toLowerCase()
+    let st2 = str2.toLowerCase()
+    return (st1 === st2) 
+}
+
+document.getElementById("chatName").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") { // Check if Enter is pressed
+        findChat();
+    }
+});
 // Function to Find Chat
 function findChat() {
     let Name = document.getElementById('chatName').value.trim();
     if (Name !== "") {
-        for (let index of names) {
-            if (index.trim() === Name) {
-                openChat(names.indexOf(index))
+        for (let name of names) {
+            if (isEqualIgnoreCase(name,Name)) {
+                openChat(names.indexOf(name))
                 document.getElementById("chatName").value = ""
                 break;
             }
